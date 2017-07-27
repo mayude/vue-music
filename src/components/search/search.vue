cf7eba5e2ab0d894abcba6183381eb34f290eadf
momozjm <739275625@qq.com>
2017-07-27 11:16:19 +0800

    vue spa
    基于vue2的音乐spa

---
+++ src/components/search/search.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="search">
+  
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+ 
+   
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+ 
+</style>
---
+++ src/components/singer/singer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="singer" ref="singer">
+    
+  </div>
+</template>
+
+<script>
+ 
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+ 
+</style>
